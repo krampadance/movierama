e1ea81ec16dc2524c2dceb2019051b29f0c14b00
vermoudas <georgios@sysunite.com>
2022-10-17 18:51:36 +0300

    Adds api callse

--- frontend/src/services/apiCalls.js
+++ frontend/src/services/apiCalls.js
@@ -3,8 +3,28 @@ import axios from 'axios';
 const api_url = "http://localhost:8000"
 
 
-export const getMovies = (skip, limit) => {
-    return axios.get(`${api_url}movies/?skip=${skip}&limit=${limit}`)
+export const getMovies = (skip, limit, order_by="none", direction="asc") => {
+    return axios.get(`${api_url}/movies/?skip=${skip}&limit=${limit}&order_by=${order_by}&direction=${direction}`)
+}
+
+export const getUserData = (token) => {
+    return axios.get(`${api_url}/users/me/`, {'headers': {'Authorization': `Bearer ${token}`}})
+}
+
+export const addMovie = (title, description, token) => {
+    return axios.post(`${api_url}/movies/`, { title, description },{'headers': {'Authorization': `Bearer ${token}`}})
+}
+
+export const addLike = (movieId, token) => {
+    return axios.post(`${api_url}/movies/${movieId}/vote/`, { likes: true },{'headers': {'Authorization': `Bearer ${token}`}})
+}
+
+export const addHate = (movieId, token) => {
+    return axios.post(`${api_url}/movies/${movieId}/vote/`, { likes: false },{'headers': {'Authorization': `Bearer ${token}`}})
+}
+
+export const getUserMovies = (userId, skip, limit, order_by, direction) => {
+    return axios.get(`${api_url}/users/${userId}/movies/?skip=${skip}&limit=${limit}&order_by=${order_by}&direction=${direction}`)
 }
 
 export const signup = (email, password, firstName, lastName) => {
@@ -16,4 +36,10 @@ export const signup = (email, password, firstName, lastName) => {
       })
 }
 
+export const login = (email, password) => {
+    return axios.post(`${api_url}/auth/login/`, {
+        username: email,
+        password: password
+      }, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+}
 
