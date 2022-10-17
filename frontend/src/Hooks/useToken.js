ff7e67c9a502a0c25f9220bd597a8678d74f658e
vermoudas <georgios@sysunite.com>
2022-10-17 18:53:37 +0300

    Adds useToken hool

--- frontend/src/Hooks/useToken.js
+++ frontend/src/Hooks/useToken.js
@@ -2,20 +2,19 @@ import { useState } from 'react';
 
 export default function useToken() {
   const getToken = () => {
-    const tokenString = sessionStorage.getItem('token');
+    const tokenString = sessionStorage.getItem('access-token');
     const userToken = JSON.parse(tokenString);
-    return userToken?.token
+    return userToken?.access_token
   };
 
   const [token, setToken] = useState(getToken());
 
   const saveToken = userToken => {
-    sessionStorage.setItem('token', JSON.stringify(userToken));
-    setToken(userToken.token);
+    sessionStorage.setItem('access-token', JSON.stringify(userToken));
+    setToken(userToken.access_token);
   };
 
   return {
-    getToken: getToken,
     setToken: saveToken,
     token
   }
